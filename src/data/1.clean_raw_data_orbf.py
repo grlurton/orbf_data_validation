c75864d7893834e96d929d809e980a2e2d134067
Grégoire Lurton <grlurton@uw.edu>
2017-01-21 11:49:43 -0800

    Select columns of interest

--- src/data/1.clean_raw_data_orbf.py
+++ src/data/1.clean_raw_data_orbf.py
@@ -9,14 +9,26 @@ print('Length Complete Data :' + str(len(data)))
 data = data[(np.isnan(data.indicator_claimed_value) == False) & (np.isnan(data.indicator_verified_value) == False) ]
 print('Length Data with claimed and verified complete :' + str(len(data)))
 
-
-
 ## Formatting facilities names for simplicity
 data.entity_name[data.entity_name == 'CNHU-PPC (Centre National Hospitalier Universitaire Pneumo Phtysiologique de Cot CSC'] = "CNHU-PPC"
-
-
 data.entity_name = data.entity_name.str.title()
+
 ## Format Date Variables
 u = data['datafile_year'].astype(str) + '-' +  data['datafile_month'].astype(str)
 data['date'] = pd.to_datetime(u)
 data['period'] = data['date'].dt.to_period('M')
+
+to_export = ['entity_id' , 'entity_name' , 'entity_type' , 'parent_geozone_name' , 'geozone_name' , 'entity_fosa_id' ,  'content' , 'entity_status' , 'entity_active' , 'filetype_name' , 'filetype_id' , 'datafile_total' , 'datafile_author_id' , 'indicator_id' , 'indicator_title' , 'indicator_claimed_value' , 'indicator_verified_value' ,   'indicator_tarif' , 'indicator_montant' , 'entity_pop' , 'entity_pop_year' , 'geozone_pop' , 'geozone_pop_year' , 'parent_geozone_pop' , 'parent_geozone_pop_year']
+
+## Qu'est-ce que entity group ?
+## Datafile status
+
+len(to_export)
+len(data.columns)
+
+
+data['datafile_status'].value_counts()
+
+out = data[]
+
+data.columns
