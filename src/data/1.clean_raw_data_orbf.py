7ae7639cd2501857e3e0841df075ae6c6ebe15f8
Grégoire Lurton <grlurton@uw.edu>
2017-01-21 11:30:47 -0800

    Data cleaning ORBF specific

--- src/data/1.clean_raw_data_orbf.py
+++ src/data/1.clean_raw_data_orbf.py
@@ -4,7 +4,12 @@ import numpy as np
 data = pd.read_csv('../../data/raw/orbf_benin.csv' , delimiter = ';')
 
 ## Keep only data with claimed and verified values
+
+print('Length Complete Data :' + str(len(data)))
 data = data[(np.isnan(data.indicator_claimed_value) == False) & (np.isnan(data.indicator_verified_value) == False) ]
+print('Length Data with claimed and verified complete :' + str(len(data)))
+
+
 
 ## Formatting facilities names for simplicity
 data.entity_name[data.entity_name == 'CNHU-PPC (Centre National Hospitalier Universitaire Pneumo Phtysiologique de Cot CSC'] = "CNHU-PPC"
