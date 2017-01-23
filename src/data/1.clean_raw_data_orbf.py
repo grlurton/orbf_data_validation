8d58e2eb2672821734dda7da389fef344690414f
Grégoire Lurton <grlurton@uw.edu>
2017-01-23 12:09:34 -0800

    colormap for indicators and facility types

--- src/data/1.clean_raw_data_orbf.py
+++ src/data/1.clean_raw_data_orbf.py
@@ -6,7 +6,7 @@ data = pd.read_csv('../../data/raw/orbf_benin.csv' , delimiter = ';')
 ## Keep only data with claimed and verified values
 
 print('Length Complete Data :' + str(len(data)))
-data = data[(np.isnan(data.indicator_claimed_value) == False) & (np.isnan(data.indicator_verified_value) == False) ]
+data = data[(np.isnan(data.indicator_claimed_value) == False) & (np.isnan(data.indicator_verified_value) == False)]
 print('Length Data with claimed and verified complete :' + str(len(data)))
 
 ## Formatting facilities names for simplicity
@@ -18,17 +18,15 @@ u = data['datafile_year'].astype(str) + '-' +  data['datafile_month'].astype(str
 data['date'] = pd.to_datetime(u)
 data['period'] = data['date'].dt.to_period('M')
 
-to_export = ['entity_id' , 'entity_name' , 'entity_type' , 'parent_geozone_name' , 'geozone_name' , 'entity_fosa_id' ,  'content' , 'entity_status' , 'entity_active' , 'filetype_name' , 'filetype_id' , 'datafile_total' , 'datafile_author_id' , 'indicator_id' , 'indicator_title' , 'indicator_claimed_value' , 'indicator_verified_value' ,   'indicator_tarif' , 'indicator_montant' , 'entity_pop' , 'entity_pop_year' , 'geozone_pop' , 'geozone_pop_year' , 'parent_geozone_pop' , 'parent_geozone_pop_year']
+to_export = ['entity_id' , 'entity_name' , 'entity_type' , 'parent_geozone_name' , 'geozone_name' , 'entity_fosa_id' ,  'content' , 'entity_status' , 'entity_active' , 'filetype_name' , 'filetype_id' , 'datafile_total' , 'datafile_author_id' , 'indicator_id' , 'indicator_title' , 'indicator_claimed_value' , 'indicator_verified_value' ,   'indicator_tarif' , 'indicator_montant' , 'entity_pop' , 'entity_pop_year' , 'geozone_pop' , 'geozone_pop_year' , 'parent_geozone_pop' , 'parent_geozone_pop_year' , 'period' , 'date']
 
-## Qu'est-ce que entity group ?
-## Datafile status
-
-len(to_export)
-len(data.columns)
+data_out = data[to_export]
 
 
-data['datafile_status'].value_counts()
+tab_c = pd.pivot_table(data , index = 'indicator_id' , columns = 'indicator_title' , aggfunc = 'count')
 
-out = data[]
+print('Exporting the Data in HD5 file')
 
-data.columns
+store = pd.HDFStore('../../data/processed/orbf_benin.h5')
+store['data'] = data_out
+store.close()
--- src/data/XX.make_references.py
+++ src/data/XX.make_references.py
@@ -8,17 +8,29 @@ store = pd.HDFStore('../../data/processed/orbf_benin.h5')
 data = store['data']
 store.close()
 
-
-colormap = pd.read_csv('../../references/color_list.csv')['color_list'].tolist()
-
+colormap_dept = pd.read_csv('../../references/color_list.csv')['departements'].tolist()
+colormap_indicators = pd.read_csv('../../references/color_list.csv')['indicateurs'].tolist()
+color_dico = {}
 
 #### Create color map for Departements
-
-order_zones = pd.DataFrame({'order':np.argsort(data.geozone_parentid.unique()) , 'zone_id':data.geozone_parentid.unique()})
+order_zones = pd.DataFrame({'order':np.argsort(data.geozone_name.unique()) , 'departement':data.geozone_name.unique()})
 zones = sorted(data.parent_geozone_name.unique().tolist())
-zone_color_dico = {}
 for u in range(len(zones)) :
-    zone_color_dico[zones[u]] = colormap[u]
+    zone_color_dico[zones[u]] = colormap_dept[u]
+
+#### Create color map for Facility Type
+order_types = pd.DataFrame({'order':np.argsort(data.entity_type.unique()) , 'type':data.entity_type.unique()})
+zones = sorted(data.entity_type.unique().tolist())
+for u in range(len(order_types)) :
+    zone_color_dico[zones[u]] = colormap_dept[u]
+
+#### Create color map for Indicators
+
+order_indicators = pd.DataFrame({'order':np.argsort(data.indicator_id.unique()) , 'indicator':data.indicator_id.unique()})
+zones = sorted(data.indicator_id.unique().tolist())
+for u in range(len(order_indicators)) :
+    print(u)
+    zone_color_dico[zones[u]] = colormap_indicators[u]
 
 with open('../../references/departements_colors.json', 'w') as outfile:
     json.dump(zone_color_dico, outfile)
