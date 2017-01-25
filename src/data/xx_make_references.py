2bb503aac9e0d1b362fccc4ac653a96791d8d73f
Grégoire Lurton <grlurton@uw.edu>
2017-01-25 12:11:21 -0800

    streamlining data cleaning
    Update make_dataset to call other scripts

--- src/data/xx_make_references.py
+++ src/data/xx_make_references.py
@@ -10,27 +10,19 @@ store.close()
 
 colormap_dept = pd.read_csv('../../references/color_list.csv')['departements'].tolist()
 colormap_indicators = pd.read_csv('../../references/color_list.csv')['indicateurs'].tolist()
-color_dico = {}
 
+color_dico = {}
 #### Create color map for Departements
 order_zones = pd.DataFrame({'order':np.argsort(data.geozone_name.unique()) , 'departement':data.geozone_name.unique()})
 zones = sorted(data.parent_geozone_name.unique().tolist())
 for u in range(len(zones)) :
-    zone_color_dico[zones[u]] = colormap_dept[u]
+    color_dico[zones[u]] = colormap_dept[u]
 
 #### Create color map for Facility Type
 order_types = pd.DataFrame({'order':np.argsort(data.entity_type.unique()) , 'type':data.entity_type.unique()})
 zones = sorted(data.entity_type.unique().tolist())
 for u in range(len(order_types)) :
-    zone_color_dico[zones[u]] = colormap_dept[u]
-
-#### Create color map for Indicators
-
-order_indicators = pd.DataFrame({'order':np.argsort(data.indicator_id.unique()) , 'indicator':data.indicator_id.unique()})
-zones = sorted(data.indicator_id.unique().tolist())
-for u in range(len(order_indicators)) :
-    print(u)
-    zone_color_dico[zones[u]] = colormap_indicators[u]
+    color_dico[zones[u]] = colormap_dept[u]
 
 with open('../../references/departements_colors.json', 'w') as outfile:
-    json.dump(zone_color_dico, outfile)
+    json.dump(color_dico, outfile)
