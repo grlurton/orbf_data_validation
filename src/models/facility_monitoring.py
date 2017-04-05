f3ff7e5a2644d7918aa6e78fa99620dc5ffb7071
Grégoire Lurton <grlurton@uw.edu>
2017-04-05 14:44:48 -0700

    Making initial training sets for facilities

--- src/models/facility_monitoring.py
+++ src/models/facility_monitoring.py
@@ -13,15 +13,38 @@ class facility(object):
     """
 
     def __init__(self , data) :
-        self.validated_data = data
+        self.facility_id = data.entity_id.iloc[0]
+        self.facility_name = data.entity_name.iloc[0]
+        self.reports = self.make_reports(data)
+        self.indicators = list(data.indicator_label.unique())
 
     def monitor_new_report(self) :
         out = np.random.choice(['Validate' , 'Supervise - Data' , 'Supervise - Services' , 'Supervise - Data and Quality'])
         return out
 
-    def make_reports(self):
+    def make_reports(self , data):
         reports = {}
         for month in list(data.date.unique()) :
             reports[str(month)[:7]] = report(data[data.date == month])
-        self.reports = reports
         return reports
+
+    def initiate_training_set(self , date):
+        training_set = {}
+        report_months = pd.Series(list(self.reports.keys()))
+        training_months = list(pd.Series(list(self.reports.keys()))[report_months < date])
+        for indic in self.indicators :
+            training_set[indic] = pd.Series([])
+            for month in training_months :
+                report = self.reports[month].report_data
+                if indic in report.index:
+                    rep_month = pd.Series([report.loc[indic , 'indicator_claimed_value']] , index =[ month])
+                    training_set[indic] = training_set[indic].append(rep_month)
+        self.training_set = training_set
+
+
+fac1 = facility(data_orbf[data_orbf.entity_id == 2])
+fac1.initiate_training_set('2013-12')
+
+
+
+data_orbf.columns
