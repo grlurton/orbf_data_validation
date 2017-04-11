1c9e4fa82e0a8b6e4e816b3a3a38e93105941882
Grégoire Lurton <grlurton@uw.edu>
2017-04-10 17:11:42 -0700

    Adding tarifs list in data processing

--- src/data/make_dataset.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-import a1_clean_raw_data_orbf
-import xx_make_references
--- src/data_preparation/xx_make_references.py
+++ src/data_preparation/xx_make_references.py
@@ -8,6 +8,7 @@ store = pd.HDFStore('../../data/processed/orbf_benin.h5')
 data = store['data']
 store.close()
 
+
 colormap_dept = pd.read_csv('../../references/color_list.csv')['departements'].tolist()
 colormap_indicators = pd.read_csv('../../references/color_list.csv')['indicateurs'].tolist()
 
@@ -26,3 +27,11 @@ for u in range(len(order_types)) :
 
 with open('../../references/departements_colors.json', 'w') as outfile:
     json.dump(color_dico, outfile)
+
+## Make tarifs as a List
+tarifs = []
+for i in data_orbf.indicator_label.unique() :
+    tarifs.append(data.indicator_tarif[data.indicator_label == i].tolist()[0])
+tarifs = pd.Series(tarifs , index = data.indicator_label.unique())
+
+tarifs.to_hdf('../../data/processed/orbf_benin.h5' , 'tarifs')
