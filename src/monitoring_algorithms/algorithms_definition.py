bd9757ba7649bad4673f49ab34b7ac287ccbba42
Grégoire Lurton <grlurton@uw.edu>
2017-04-28 16:44:57 -0700

    Training data updated to work with DataFrame reports from facilities

--- src/monitoring_algorithms/algorithms_definition.py
+++ src/monitoring_algorithms/algorithms_definition.py
@@ -50,42 +50,38 @@ class monitoring_algorithm(object):
         facility_name = facility_data.facility_name
         departement = facility_data.departement
         supervisions = facility_data.supervisions
-        reports_months = np.array(list(facility_data.reports.keys()))
+        reports_months = facility_data.reports.index.levels[0]
+        training_months = reports_months[reports_months < mois]
         if algorithm_name not in list(supervisions.columns) :
-            training_months = sorted(list(reports_months[reports_months < mois]))
             supervisions = supervisions.append(pd.DataFrame(['Initial Training']*len(training_months) , index = training_months , columns = [algorithm_name]))
             if self.transversal == False :
                 self.facility_data.supervisions = supervisions
             if self.transversal == True :
                 index_fac = self.list_name_facilites.index(facility_name)
                 self.facility_data[index_fac].supervisions = supervisions
-
-        if (algorithm_name in list(supervisions.columns)) & (mois in reports_months) :
-            training_months =  sorted(list(supervisions.index[~supervisions[algorithm_name].isnull()]))
-            validated_data = []
-            for month in training_months :
-                month_report = facility_data.reports[month].report_data
+        if (algorithm_name in list(supervisions.columns)) :
+            verified_months =  supervisions.index[supervisions[algorithm_name].isin([True , 'Initial Training'])]
+            unverified_months = supervisions.index[supervisions[algorithm_name] == False]
+            verified_data =  pd.DataFrame([] , index = [])
+            claimed_data =  pd.DataFrame([] , index = [])
+            if (len(list(verified_months)) > 0) | (type(verified_months) == 'Period') :
+                verified_data = facility_data.reports.loc[list(verified_months) , ['indicator_claimed_value'  , 'indicator_verified_value' , 'indicator_tarif']]
+                verified_data.columns = ['indicator_claimed_value' ,'indicator_validated_value' , 'indicator_tarif']
+            if len(list(unverified_months)) > 0 | (type(unverified_months) == 'Period') :
+                print(facility_name)
+                print(unverified_months)
                 try :
-                    month_status = supervisions.loc[month , algorithm_name]
-                except KeyError :
-                    pass ## Some facilities miss some reports. Should be solved when passing to a central DataFrame
-                if month_status in [True , 'Initial Training']  :
-                    month_data = month_report[['indicator_verified_value' , 'indicator_tarif']]
-                if month_status == False :
-                    month_data = month_report[['indicator_claimed_value' , 'indicator_tarif']]
-                month_data.columns = ['indicator_validated_value' , 'indicator_tarif']
-                if self.validation_trail == True :
-                    month_data['indicator_claimed_value'] = month_report['indicator_claimed_value']
-                    month_data['verification_status'] = month_status
-                month_data['date'] = month
-                month_data['facility_name'] = facility_name
-                month_data['departement'] = departement
-                month_data = month_data.set_index(['departement' , 'facility_name' , 'date'] , append= True).reorder_levels(['departement' , 'facility_name' , 'date' , 'indicator_label'])
-                if len(validated_data) > 0 :
-                    validated_data = validated_data.append(month_data)
-                if len(validated_data) == 0 :
-                    validated_data = month_data
-            validated_data = validated_data.sort_index()
+                    claimed_data = facility_data.reports.loc[list(unverified_months) , ['indicator_claimed_value'  , 'indicator_claimed_value' , 'indicator_tarif']]
+                    claimed_data.columns = ['indicator_claimed_value' , 'indicator_validated_value' , 'indicator_tarif']
+
+                except( KeyError , TypeError ):
+                    pass
+
+        validated_data = verified_data.append(claimed_data)
+        validated_data['facility_name'] = facility_name
+        validated_data['departement'] = departement
+        if len(validated_data) > 0 :
+            validated_data = validated_data.reset_index().set_index(['departement' , 'facility_name' , 'period'  , 'indicator_label']).reorder_levels(['departement' , 'facility_name' , 'period' , 'indicator_label']).sort_index()
             return validated_data
 
     def make_transversal_training_set(self , data , mois):
@@ -98,10 +94,11 @@ class monitoring_algorithm(object):
     def simulate_implementation(self , date_start , date_stop  , data , **kwargs):
         if self.transversal == True :
             def date_range(fac):
-                return list(fac.reports.keys())
-            dates = list(map(date_range , data))
-            dates =  [item for sublist in dates for item in sublist]
-            dates = np.array(list(set(dates)))
+                return fac.reports.index.levels[0]
+            full_dates = list(map(date_range , data))
+            dates = full_dates[0]
+            for n in range(1,len(full_dates)):
+                dates.union(full_dates[n])
             months_to_screen = sorted(dates[(dates >= date_start) & (dates <= date_stop)])
         screening_method = self.monitor
         trigger_method = self.trigger_supervisions
@@ -126,8 +123,8 @@ pkl_file.close()
 def simulate_aedes(screening_method , trigger_method , data , dates , **kwargs):
     for date in dates :
         print(date)
-        month = date[5:7]
-        if month in ['01' , '07']:
+        month = date.month
+        if month in [1 ,7]:
             print('Time to make a classification')
             screening_method(data , mois = date , **kwargs)
         aedes_algorithm.trigger_supervisions(date)
@@ -140,11 +137,7 @@ aedes_algorithm = monitoring_algorithm('aedes' , screen_function , draw_supervis
 
 aedes_algorithm.simulate_implementation('2013-01' , '2016-12', facilities , **kwargs)
 
-
-
-
-
-get_facility(facilities , 'Dedekpoe Csc').reports
+aedes_algorithm.list_name_facilites
 
 %matplotlib inline
 
@@ -169,6 +162,4 @@ for i in range(1,17):
     departement =departement.replace('’' , "'")
     plt.title(departement , fontsize=15)
 
-
-## TODO Store Report Data dans un dataFrame by facility
-## TODO Add an export and storage of the status in the facility object
+## TODO Add an export and storage of the status in the facility object => screening_trace
