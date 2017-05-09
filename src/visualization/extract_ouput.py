1febc1cb58faa55fa50dbdd41e5afa9deeb61641
Grégoire Lurton <grlurton@uw.edu>
2017-05-09 12:15:33 -0700

    adding full supervision numbers + taking out facilities missing

--- src/visualization/extract_ouput.py
+++ src/visualization/extract_ouput.py
@@ -15,16 +15,11 @@ def get_verification_path(facility_data , algorithm_name):
     supervisions = facility_data.supervisions.copy()
     facility_name = facility_data.facility_name
     departement = facility_data.departement
-    if algorithm_name not in list(supervisions.columns) :
-        pass
-        #print('Algorithm ' + algorithm_name + ' does not seem to have been run on ' + facility_name)
-
     if (algorithm_name in list(supervisions.columns)) :
         supervisions = pd.DataFrame(supervisions[algorithm_name])
         supervisions = supervisions[~pd.isnull(supervisions[algorithm_name])]
         verified_months =  supervisions.index[supervisions[algorithm_name].isin([True , 'Initial Training'])]
         unverified_months = supervisions.index[supervisions[algorithm_name] == False]
-        print(unverified_months)
         verified_data =  pd.DataFrame([] , index = [])
         claimed_data =  pd.DataFrame([] , index = [])
 
@@ -60,35 +55,53 @@ def get_verification_path(facility_data , algorithm_name):
             print('Did not Work')
             pass
 
-
-## FIXME Drop altogteher facilities for which at which one algorithm does not come through.
-## TODO groupby this so it can extract different algorithms pathes
-## TODO Need to add a default full supervision path
-
-## TODO Have a classification of reports as in or out of control :
-## IDEA Need more brainstorm on what in and out of control
-
-facilities[2].supervisions
-
 def valid_param(fac):
     return get_verification_path(fac , 'aedes_50')
-validation_path = list(map(valid_param , facilities))
-aedes_data_50 = validation_path[0].append(validation_path[1:len(validation_path)])
-
+validation_path_5 = list(map(valid_param , facilities))
+aedes_data_50 = pd.concat(validation_path_5)
 
 def valid_param(fac):
     return get_verification_path(fac , 'aedes_80')
-validation_path = list(map(valid_param , facilities))
+validation_path_8 = list(map(valid_param , facilities))
+aedes_data_80 = pd.concat(validation_path_8)
 
-aedes_data_80 = validation_path[0].append(validation_path[1:len(validation_path)])
+def make_full_verif(facility_data):
+    facility_name = facility_data.facility_name
+    departement = facility_data.departement
+    data = facility_data.reports[['indicator_claimed_value'  , 'indicator_verified_value' , 'indicator_tarif' , 'indicator_verified_value']]
+    data.columns = ['indicator_claimed_value' ,'indicator_validated_value' , 'indicator_tarif'  , 'indicator_verified_value']
+    data['facility_name'] = facility_name
+    data['departement'] = departement
+    data['trigger'] = True
+    data['algorithm'] = 'full_verification'
+    data = data.reset_index().set_index(['algorithm' , 'departement' , 'facility_name' , 'period'  , 'indicator_label']).reorder_levels(['algorithm' , 'departement' , 'facility_name' ,  'period' , 'indicator_label']).sort_index()
+    return data
 
-aedes_data = aedes_data_50.append(aedes_data_80)
 
-out = open('../../data/processed/TEMP_aedes_fulldata.pkl' , 'wb')
-pickle.dump(aedes_data , out , pickle.HIGHEST_PROTOCOL)
-out.close()
+full_data = list(map(make_full_verif , facilities))
+full_data_df = pd.concat(full_data)
+aedes_data = full_data_df.append(aedes_data_80).append(aedes_data_50)
+aedes_data.sortlevel(inplace = True)
 
-aedes_data
+def keep_only_full_facilities(df_algos) :
+    idx = pd.IndexSlice
+    full_facs = list(df_algos.index.levels[2])
+    print(len(full_facs))
+    for algo in list(df_algos.index.levels[0]) :
+        print(algo)
+        algo_data  = df_algos.loc[algo].reset_index()
+        facs = list(algo_data['facility_name'].unique())
+        full_facs = [x for x in full_facs if x in facs]
+        print(len(full_facs))
+    df_full = df_algos.loc[idx[:, :, full_facs] , :]
+    return df_full
+
+out = keep_only_full_facilities(aedes_data)
+
+
+## TODO Have a classification of reports as in or out of control :
+## IDEA Need more brainstorm on what in and out of control
+## TODO this script should be standalone to get all simulated algorithms and return a dataframe with all plotting data
 
 
 ## Additional function for interesting parameters
@@ -105,23 +118,41 @@ def get_interesting_quantities(validated_path):
     validated_path['undue_payment_made'] = validated_path['validated_payment'] - validated_path['verified_payment']
     return validated_path
 
+
+
+
+
+## FIXME Need different supervision cost depending on the facility type
+
+
 mean_supervision_cost = 170000
 
-payments_description= get_interesting_quantities(aedes_data)
+def make_plot_dataset(aedes_data , mean_supervision_cost):
+    payments_description= get_interesting_quantities(aedes_data)
+    aedes_monthly_verifications = aedes_data.groupby(level = 3).apply(count_supervisions)
+    data_out = pd.DataFrame(aedes_monthly_verifications , columns = ['monthly_verifications'])
+    data_out['supervision_costs'] = data_out['monthly_verifications'] * mean_supervision_cost
+    aedes_total_payment = payments_description['validated_payment'].groupby(level = 3).sum()
+    data_out['total_payment'] = payments_description['validated_payment'].groupby(level = 3).sum()
+    data_out['share_supervision_cost'] = data_out['supervision_costs'] / data_out['total_payment']
+    data_out['undue_payment'] = payments_description['undue_payment_made'].groupby(level = 3).sum()
+    return data_out
 
-aedes_monthly_verifications = aedes_data.groupby(level = 3).apply(count_supervisions)
-data_out = pd.DataFrame(aedes_monthly_verifications , columns = ['monthly_verifications'])
-data_out['supervision_costs'] = data_out['monthly_verifications'] * mean_supervision_cost
-aedes_total_payment = payments_description['validated_payment'].groupby(level = 3).sum()
-data_out['total_payment'] = payments_description['validated_payment'].groupby(level = 3).sum()
-data_out['share_supervision_cost'] = data_out['supervision_costs'] / data_out['total_payment']
-data_out['undue_payment'] = payments_description['undue_payment_made'].groupby(level = 3).sum()
+data_out = aedes_data.groupby(level = 0).apply(make_plot_dataset , mean_supervision_cost)
 
-data_out = data_out[data_out['total_payment'] < 1e9]
+
+#data_out = data_out[data_out['undue_payment'] < 1e8]
+
+out = open('../../data/processed/TEMP_aedes_fulldata.pkl' , 'wb')
+pickle.dump(data_out , out , pickle.HIGHEST_PROTOCOL)
+out.close()
 
 import matplotlib.pyplot as plt
 
 %matplotlib inline
-plt.plot(data_out['supervision_costs'].tolist() , data_out['undue_payment'].tolist() , 'o')
+
+plt.plot(data_out.loc['full_verification' , 'share_supervision_cost'].tolist() , data_out.loc['full_verification' , 'undue_payment'].tolist() , 'o')
+plt.plot(data_out.loc['aedes_50' , 'share_supervision_cost'].tolist() , data_out.loc['aedes_50' , 'undue_payment'].tolist() , 'o')
+plt.plot(data_out.loc['aedes_80' , 'share_supervision_cost'].tolist() , data_out.loc['aedes_80' , 'undue_payment'].tolist() , 'or')
 
 ######
